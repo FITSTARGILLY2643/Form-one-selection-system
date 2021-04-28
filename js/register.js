415120fdd7c5a3b2b107b635f3af8ec9339b45eb
FISTARGILLY2643 <fitstargilii1194@gmail.com>
2021-04-29 00:25:18 +0300

    Validate register and login form

--- js/register.js
+++ js/register.js
@@ -1,66 +1,39 @@
-var student = [{
-        index: "1194",
-        password: "1234"
-    },
-    {
-        index: "2643",
-        password: "0000"
-    },
-    {
-        index: "1950",
-        password: "1111"
+window.onload = (event) => {
+
+    function User(fname, lname, email, index, password, cpassword) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.index = index;
+        this.password = password;
+        this.cpassword = cpassword;
+
+    }
+    var firstName = document.querySelector('#fname');
+    var lastName = document.querySelector('#lname');
+    var studentEmail = document.querySelector('#email');
+    const studentIndex = document.querySelector('#index');
+    const studentPassword = document.querySelector('#password');
+    var studentCpassword = document.querySelector('#cpassword');
+    var regbtn = document.querySelector('#register');
+
+
+    regbtn.addEventListener('submit', register);
+
+    function register(e) {
+        var newUser = new User(firstName.value, lastName.value, studentEmail.value, studentIndex.value, studentPassword.value, studentCpassword.value);
+
+
+        if (firstName.value === '' || lastName.value === '' || studentEmail.value === "" || studentIndex.value === "" || studentPassword.value === "" || studentCpassword.value === "")
+            alert('All fields are required')
+
+
+        localStorage.setItem('user', JSON.stringify(newUser))
+
+
+        // e.preventDefault();
     }
 
 
-]
-
-
-
-function getInfo() {
-    var index = document.getElementById("index-L").value;
-    var password = document.getElementById("password-L").value
-    for (i = 0; i < student.length; i++) {
-        if (index == student[i].index && password == student[i].password) {
-            alert("You are logged in");
-
-            //     // return true;
-            // } else {
-            //     alert("Incorrect credentials")
-            //         // return false;
-            // }
-        }
-
-    }
 
 }
-
-// function Student(index, password) {
-//     this.studentIndex = index;
-//     this.studentPassword = password;
-
-// }
-
-// $(document).ready(function() {
-
-//             $("form#myform").submit(function(event) {
-//                     event.preventDefault();
-
-//                     // Get register details
-//                     var inputtedIndex = $("input#index").val();
-//                     var inputtedPassword = $("input#password1").val();
-//                     var newStudent = new Student(inputtedIndex, inputtedPassword);
-
-//                     // Get login details
-//                     var index = document.getElementById("index-L").value;
-//                     var password = document.getElementById("password-L").value
-
-//                     if (index == newStudent.index && password == newStudent.password) {
-//                         alert("You are logged in");
-
-
-//                     }
-
-//                 }
-
-
-//             });
