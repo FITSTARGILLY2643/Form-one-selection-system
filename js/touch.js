7d66aa593e6a54fb345a4813832bd503a71a38ce
diana3664 <dianamus2017@gmail.com>
2021-04-29 12:58:53 +0300

    add an alert for low grades

--- js/touch.js
+++ js/touch.js
@@ -8,7 +8,16 @@ $("#column_select").change(function () {
         $("#layout_select").find("option:visible:first").val());
 
         $("button").click(function(event){
-          $("#box").show();
+          var marks = $("#marks").val();
+          var region = $("#layout_select").val();
+          var school = $("#column_select").val();
+          if (marks <= 350) {
+            alert('low marks')
+          }else if (marks >= 500) {
+            alert('invalid marks')
+          }
+          else(
+          $("#box").show());
           event.preventDefault()
         });
 
